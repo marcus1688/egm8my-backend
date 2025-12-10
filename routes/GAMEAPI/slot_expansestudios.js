ea8f1de74abe552198a4b90371d0c155f4f76074
bolt1688 <mrcmrc3399@gmail.com>
2025-12-10 13:05:33 +0800

    Update slot_expansestudios.js

--- routes/GAMEAPI/slot_expansestudios.js
+++ routes/GAMEAPI/slot_expansestudios.js
@@ -685,7 +685,7 @@ const toDecimal = (value) => new Decimal(Number(value) || 0).toDecimalPlaces(4);
 
 router.post("/api/expansestudios/transaction", async (req, res) => {
   const { requestId, brandId, playerId, trans, gameType } = req.body;
-
+  console.log(trans, "hi");
   if (!requestId || !brandId || !playerId || !trans?.length) {
     return res.status(200).json({
       requestId: requestId || "",
